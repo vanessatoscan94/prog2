fb9da945120659d121c4027b7e9eadc535875c2b
vanessatoscan94 <23423423+username@blubber.com>
2020-03-06 10:34:52 +0100

    das ist ein test

---
+++ prog2/app/test.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask("Demo")
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def begruessung(name=False):
+    if name:
+         return "Hallo " + name + "!"
+    else:
+        return "Not Hallo World again…"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
